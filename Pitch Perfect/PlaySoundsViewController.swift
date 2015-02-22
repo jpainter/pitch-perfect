f976af62547ab1d98b59cbd045ba0a607e760168
John Painter <painter.ja@gmail.com>
2015-02-22 10:38:31 -0500

    added chipmunk and darth sound effects color background of player

--- Pitch Perfect/PlaySoundsViewController.swift
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -13,6 +13,8 @@ class PlaySoundsViewController: UIViewController {
     
     var audioPlayer: AVAudioPlayer!
     var receivedAudio:RecordedAudio!
+    var audioEngine: AVAudioEngine!
+    var audioFile:AVAudioFile!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,6 +32,8 @@ class PlaySoundsViewController: UIViewController {
         
         audioPlayer = AVAudioPlayer(contentsOfURL: receivedAudio.filePathURL, error: nil)
         audioPlayer.enableRate = true
+        audioEngine = AVAudioEngine()
+        audioFile = AVAudioFile(forReading: receivedAudio.filePathURL , error: nil)
 
             }
 
@@ -53,6 +57,40 @@ class PlaySoundsViewController: UIViewController {
         audioPlayer.rate = 2.0
         audioPlayer.play()
     }
+    
+    @IBAction func playChipmunk(sender: UIButton) {
+        
+        playAudioWithVariablePitch(1000)
+        
+    }
+    
+    @IBAction func playDarthVader(sender: UIButton) {
+        playAudioWithVariablePitch(-1000)
+        
+    }
+
+    
+    func playAudioWithVariablePitch(pitch: Float){
+        audioPlayer.stop()
+        audioEngine.stop()
+        audioEngine.reset()
+        
+        var audioPlayerNode = AVAudioPlayerNode()
+        audioEngine.attachNode(audioPlayerNode)
+        
+        var changePitchEffect = AVAudioUnitTimePitch()
+        changePitchEffect.pitch = pitch
+        audioEngine.attachNode(changePitchEffect)
+        
+        audioEngine.connect(audioPlayerNode, to: changePitchEffect, format: nil)
+        audioEngine.connect(changePitchEffect, to: audioEngine.outputNode, format: nil)
+        
+        audioPlayerNode.scheduleFile(audioFile, atTime: nil, completionHandler: nil)
+        audioEngine.startAndReturnError(nil)
+        
+        audioPlayerNode.play()
+    }
+    
     
     @IBAction func stopPlay(sender: UIButton) {
         audioPlayer.stop()
